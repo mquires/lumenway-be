31b25382104aec3fb2ea8f7b2f11cee4460ec3e2
mquires <evg.tulya@gmail.com>
2025-03-01 01:11:30 +0300

    feat: add updating email and password of profile

--- src/modules/auth/account/inputs/create-user.input.ts
+++ src/modules/auth/account/inputs/create-user.input.ts
@@ -9,19 +9,19 @@ import {
 
 @InputType()
 export class CreateUserInput {
-  @Field()
+  @Field(() => String)
   @IsString()
   @IsNotEmpty()
   @Matches(/^[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*$/)
   public username: string;
 
-  @Field()
+  @Field(() => String)
   @IsString()
   @IsNotEmpty()
   @IsEmail()
   public email: string;
 
-  @Field()
+  @Field(() => String)
   @IsString()
   @IsNotEmpty()
   @MinLength(8)

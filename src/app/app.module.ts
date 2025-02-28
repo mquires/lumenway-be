378384721892ecd542a1c4fecf497be2bede4ea6
mquires <evg.tulya@gmail.com>
2025-03-01 02:05:41 +0300

    feat: configure s3 storage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TotpModule } from '../modules/auth/totp/totp.module';
 import { VerificationModule } from '../modules/auth/verification/verification.module';
 import { CronModule } from '../modules/cron/cron.module';
 import { MailModule } from '../modules/libs/mail/mail.module';
+import { S3Module } from '../modules/libs/s3/s3.module';
 import { IS_DEV_ENV } from '../shared/utils/is-dev.util';
 
 import { getGraphqlConfig } from './config/graphql.config';
@@ -39,6 +40,7 @@ import { RedisModule } from './redis/redis.module';
     TotpModule,
     DeactivateModule,
     CronModule,
+    S3Module,
   ],
 })
 export class AppModule {}
